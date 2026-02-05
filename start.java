3aafb855aa28a570e573727494c0481cc6ba0318
Ridwan2024-P <ahsanrid@gmail.com>
2026-02-05 19:02:01 +0600

    basic 50%

--- start.java
+++ start.java
@@ -1,5 +1,57 @@
 public class start{
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        String name="Ridwan Bin Ahsan";
+        System.out.println("My name is: " + name);
+        System.out.println("Hello World!");
+System.out.println("Have a good day!");
+System.out.println("Learning Java is fun!");
+System.out.print("Hello World! ");
+System.out.print("I will print on the same line.");
+System.out.println(3);
+System.out.println(358);
+System.out.println(50000);
+System.out.println(3 + 3);
+System.out.println(2 * 5);
+//single line comment
+/* This is a multi-line comment
+   that spans multiple lines */
+   final int myNum = 15;
+  // myNum = 20; // This will cause an error
+    System.out.println(myNum);
+
+    int x = 5;
+  ///  x = 10;
+    int y = 7;
+   // x = y;
+   System.out.println("The value of x is: " + x+y);
+    System.out.println("The Sum of x and y is: " + (x + y));
+    int five = 10; // This is a valid variable name
+    System.out.println("The value of five is: " + five);
+    // Student data
+String studentName = "John Doe";
+int studentID = 15;
+int studentAge = 23;
+float studentFee = 75.25f;
+char studentGrade = 'B';
+
+// Print variables
+System.out.println("Student name: " + studentName);
+System.out.println("Student id: " + studentID);
+System.out.println("Student age: " + studentAge);
+System.out.println("Student fee: " + studentFee);
+System.out.println("Student grade: " + studentGrade);
+int items = 50;
+float costPerItem = 9.99f;
+float totalCost = items * costPerItem;
+char currency = '$';
+
+// Print variables
+System.out.println("Number of items: " + items);
+System.out.println("Cost per item: " + costPerItem + currency);
+System.out.println("Total cost = " + totalCost + currency);
+ 
+
+
     }
 }
