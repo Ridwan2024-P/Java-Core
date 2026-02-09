dd9f52d514aa931119fdb56ab5cdd70779b5fdc5
Ridwan2024-P <ahsanrid@gmail.com>
2026-02-09 18:13:38 +0600

    odd even

---
+++ arr.java
@@ -0,0 +1,26 @@
+public class arr {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        
+
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 != 0) {
+                System.out.println("Odd element: " + arr[i]);
+            }
+        }
+       
+    
+    
+    for(int i=0;i<arr.length;i++){
+        if(arr[i]%2==0){
+           System.out.println("Even element: " + arr[i]  ); 
+        }
+        
+    }
+    }
+
+    
+    
+
+}
