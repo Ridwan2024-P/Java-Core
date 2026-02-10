e655d95cfb51fa8fc33a94a9bd482b5e26a94ee6
Ridwan2024-P <ahsanrid@gmail.com>
2026-02-10 18:50:45 +0600

    for each loop

---
+++ foreachloop.java
@@ -0,0 +1,9 @@
+public class foreachloop{
+    public static void main(String[] args){
+    int [] numbers ={25,69,65,255,3,1,2,5,2,3,4,5,6,7,8,9,10};
+    for(int a:numbers)
+    {
+        System.out.print(a+" ");
+    }
+}
+}
